25bbab0401c56619bd66819b7765ed3341d9c92e
Hydriuk <hydriuk@gmail.com>
2022-10-16 12:58:44 +0200

    Ajoutez des fichiers projet.

---
+++ OpenMod/OpenMod.cs
@@ -0,0 +1,19 @@
+﻿using Cysharp.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using OpenMod.API.Plugins;
+using OpenMod.Unturned.Plugins;
+using System;
+
+[assembly: PluginMetadata("BuildSpawner", DisplayName = "BuildSpawner", Author = "Hydriuk")]
+
+namespace BuildSpawner.OpenMod
+{
+    public class Plugin : OpenModUnturnedPlugin
+    {
+        public Plugin(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+    }
+}
---
+++ RocketMod/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using Rocket.Core.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildSpawner.RocketMod
+{
+    public class Plugin : RocketPlugin
+    {
+        public static Plugin Instance { get; set; }
+
+        protected override void Load()
+        {
+            Instance = this;
+        }
+    }
+}
