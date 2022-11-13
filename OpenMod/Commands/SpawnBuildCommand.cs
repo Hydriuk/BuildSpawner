a8e98d067c054db8b6406a5e4a0a65e953fae17e
Hydriuk <hydriuk@gmail.com>
2022-11-13 15:36:11 +0100

    Added owner parameters

--- OpenMod/Commands/SpawnBuildCommand.cs
+++ OpenMod/Commands/SpawnBuildCommand.cs
@@ -5,6 +5,7 @@ using OpenMod.Core.Commands;
 using OpenMod.Unturned.Commands;
 using OpenMod.Unturned.Users;
 using SDG.Unturned;
+using Steamworks;
 using System;
 using UnityEngine;
 
@@ -12,7 +13,7 @@ namespace BuildSpawner.OpenMod.Commands
 {
     [Command("spawnbuild")]
     [CommandAlias("sbuild")]
-    [CommandSyntax("<name> [<shiftX> <shiftY> <shiftZ>] [-origin | -o]")]
+    [CommandSyntax("<name> [<shiftX> <shiftY> <shiftZ>] [-o] [-p <player>] [-pg <player>]")]
     [CommandDescription("Spawn a build.")]
     [CommandActor(typeof(UnturnedUser))]
     public class SpawnBuildCommand : UnturnedCommand
@@ -28,49 +29,126 @@ namespace BuildSpawner.OpenMod.Commands
         {
             UnturnedUser user = (UnturnedUser)Context.Actor;
 
-            if (Context.Parameters.Length != 1 && Context.Parameters.Length != 2 && Context.Parameters.Length != 4)
+            int i = 0;
+
+            string buildName = string.Empty;
+            if (Context.Parameters.Length > i)
+            {
+                buildName = Context.Parameters[0];
+                i++;
+            }
+
+            float shiftX = 0;
+            float shiftY = 0;
+            float shiftZ = 0;
+            if (Context.Parameters.Length > i + 3 &&
+                float.TryParse(Context.Parameters[1], out shiftX) &&
+                float.TryParse(Context.Parameters[2], out shiftY) &&
+                float.TryParse(Context.Parameters[3], out shiftZ))
+            {
+                i += 3;
+            }
+
+            bool origin = false;
+            CSteamID playerId = CSteamID.Nil;
+            CSteamID groupId = CSteamID.Nil;
+            while (i < Context.Parameters.Length)
+            {
+                // Parse origin
+                if (Context.Parameters[i] == "-origin" || Context.Parameters[i] == "-o")
+                {
+                    origin = true;
+                    i++;
+                }
+
+                // Parse player
+                else if (Context.Parameters[i] == "-player" || Context.Parameters[i] == "-p")
+                {
+                    if(i + 1 < Context.Parameters.Length && !Context.Parameters[i + 1].StartsWith("-"))
+                    {
+                        playerId = ParsePlayer(Context.Parameters[i + 1]);
+                        i += 2;
+                    }
+                    else
+                    {
+                        playerId = user.SteamId;
+                        i++;
+                    }
+                }
+
+                // Parse playergroup
+                else if (Context.Parameters[i] == "-playergroup" || Context.Parameters[i] == "-pg")
+                {
+                    if (i + 1 < Context.Parameters.Length && !Context.Parameters[i + 1].StartsWith("-"))
+                    {
+                        playerId = ParsePlayer(Context.Parameters[i + 1]);
+                        groupId = ParseGroup(Context.Parameters[i + 1]);
+                        i += 2;
+                    }
+                    else
+                    {
+                        playerId = user.SteamId;
+                        groupId = user.Player.Player.quests.groupID;
+                        i++;
+                    }
+                }
+                else
+                {
+                    throw new CommandWrongUsageException(Context);
+                }
+            }
+
+            if (Context.Parameters.Length > i ||
+                buildName == string.Empty)
                 throw new CommandWrongUsageException(Context);
 
-            string buildName = Context.Parameters[0];
-
-            if (Context.Parameters.Length == 2)
+            if (origin)
             {
-                if (!_buildManager.PlaceBuild(buildName, user.SteamId.m_SteamID, user.Player.Player.channel.owner.playerID.group.m_SteamID))
+                if (!_buildManager.PlaceBuild(buildName, playerId.m_SteamID, groupId.m_SteamID))
                     throw new UserFriendlyException($"{buildName} does not exist");
 
                 return UniTask.CompletedTask;
             }
 
             Vector3 shift;
-            if (Context.Parameters.Length == 4)
-            {
-                if (!float.TryParse(Context.Parameters[1], out float shiftX) ||
-                    !float.TryParse(Context.Parameters[2], out float shiftY) ||
-                    !float.TryParse(Context.Parameters[3], out float shiftZ))
-                {
-                    throw new CommandWrongUsageException("Shifts must be numbers");
-                }
-
+            if (shiftX != 0 || shiftY != 0 || shiftZ != 0)
                 shift = new Vector3(shiftX, shiftY, shiftZ);
-            }
             else
-            {
                 shift = Vector3.zero;
-            }
 
             bool buildFound = _buildManager.PlaceBuild(
                 buildName,
                 user.Player.Player.transform.position,
                 user.Player.Player.transform.rotation,
                 shift,
-                user.SteamId.m_SteamID,
-                user.Player.Player.channel.owner.playerID.group.m_SteamID
+                playerId.m_SteamID,
+                groupId.m_SteamID
             );
 
             if (!buildFound)
                 throw new UserFriendlyException($"{buildName} does not exist");
 
             return UniTask.CompletedTask;
+        }
+
+        private CSteamID ParsePlayer(string name)
+        {
+            Player player = PlayerTool.getPlayer(name);
+
+            if (player == null)
+                throw new CommandWrongUsageException($"Player \"{name}\" not found");
+
+            return player.channel.owner.playerID.steamID;
+        }
+
+        private CSteamID ParseGroup(string name)
+        {
+            Player player = PlayerTool.getPlayer(name);
+
+            if (player == null)
+                throw new CommandWrongUsageException($"Player \"{player}\" not found");
+
+            return player.quests.groupID;
         }
     }
 }
--- RocketMod/Commands/SpawnBuildCommand.cs
+++ RocketMod/Commands/SpawnBuildCommand.cs
@@ -1,7 +1,9 @@
 ﻿using Rocket.API;
 using Rocket.Unturned.Player;
 using SDG.Unturned;
+using Steamworks;
 using System.Collections.Generic;
+using System.Runtime.Remoting.Contexts;
 using UnityEngine;
 
 namespace BuildSpawner.RocketMod.Commands
@@ -22,51 +24,147 @@ namespace BuildSpawner.RocketMod.Commands
 
         public void Execute(IRocketPlayer caller, string[] command)
         {
-            UnturnedPlayer player = (UnturnedPlayer)caller;
+            UnturnedPlayer uPlayer = (UnturnedPlayer)caller;
 
-            if (command.Length != 1 && command.Length != 2 && command.Length != 4)
+            int i = 0;
+
+            string buildName = string.Empty;
+            if (command.Length > i)
             {
-                ChatManager.serverSendMessage($"Wrong syntax: {Syntax}", Color.red, toPlayer: player.SteamPlayer());
+                buildName = command[0];
+                i++;
+            }
+
+            float shiftX = 0;
+            float shiftY = 0;
+            float shiftZ = 0;
+            if (command.Length > i + 3 &&
+                float.TryParse(command[1], out shiftX) &&
+                float.TryParse(command[2], out shiftY) &&
+                float.TryParse(command[3], out shiftZ))
+            {
+                i += 3;
+            }
+
+            bool origin = false;
+            CSteamID playerId = CSteamID.Nil;
+            CSteamID groupId = CSteamID.Nil;
+            while (i < command.Length)
+            {
+                // Parse origin
+                if (command[i] == "-origin" || command[i] == "-o")
+                {
+                    origin = true;
+                    i++;
+                }
+
+                // Parse player
+                else if (command[i] == "-player" || command[i] == "-p")
+                {
+                    if (i + 1 < command.Length && !command[i + 1].StartsWith("-"))
+                    {
+                        playerId = ParsePlayer(command[i + 1]);
+
+                        if (playerId == CSteamID.Nil)
+                        {
+                            ChatManager.serverSendMessage($"Player \"{command[i + 1]}\" not found", Color.red, toPlayer: uPlayer.SteamPlayer());
+                            return;
+                        }
+
+                        i += 2;
+                    }
+                    else
+                    {
+                        playerId = uPlayer.CSteamID;
+                        i++;
+                    }
+                }
+
+                // Parse playergroup
+                else if (command[i] == "-playergroup" || command[i] == "-pg")
+                {
+                    if (i + 1 < command.Length && !command[i + 1].StartsWith("-"))
+                    {
+                        playerId = ParsePlayer(command[i + 1]);
+
+                        if(playerId == CSteamID.Nil)
+                        {
+                            ChatManager.serverSendMessage($"Player \"{command[i + 1]}\" not found", Color.red, toPlayer: uPlayer.SteamPlayer());
+                            return;
+                        }
+
+                        groupId = ParseGroup(command[i + 1]);
+                        i += 2;
+                    }
+                    else
+                    {
+                        playerId = uPlayer.CSteamID;
+                        groupId = uPlayer.Player.quests.groupID;
+                        i++;
+                    }
+                }
+                else
+                {
+                    ChatManager.serverSendMessage($"Wrong syntax: {Syntax}", Color.red, toPlayer: uPlayer.SteamPlayer());
+                    return;
+                }
+            }
+
+            if (command.Length > i ||
+                buildName == string.Empty)
+            {
+                ChatManager.serverSendMessage($"Wrong syntax: {Syntax}", Color.red, toPlayer: uPlayer.SteamPlayer());
                 return;
             }
 
-            string buildName = command[0];
-
-            if (command.Length == 2)
+            if (origin)
             {
-                if (!Plugin.Instance.BuildManager.PlaceBuild(buildName, player.CSteamID.m_SteamID, player.SteamGroupID.m_SteamID))
-                    ChatManager.serverSendMessage($"{buildName} does not exist", Color.yellow, toPlayer: player.SteamPlayer());
+                if (!Plugin.Instance.BuildManager.PlaceBuild(buildName, playerId.m_SteamID, groupId.m_SteamID))
+                    ChatManager.serverSendMessage($"{buildName} does not exist", Color.red, toPlayer: uPlayer.SteamPlayer());
 
                 return;
             }
 
             Vector3 shift;
-            if (command.Length == 4)
-            {
-                if (!float.TryParse(command[1], out float shiftX) || !float.TryParse(command[2], out float shiftY) || !float.TryParse(command[3], out float shiftZ))
-                {
-                    ChatManager.serverSendMessage("Coordinates must be decimal numbers", Color.red, toPlayer: player.SteamPlayer());
-                    return;
-                }
-
+            if (shiftX != 0 || shiftY != 0 || shiftZ != 0)
                 shift = new Vector3(shiftX, shiftY, shiftZ);
-            }
             else
-            {
                 shift = Vector3.zero;
-            }
 
             bool buildFound = Plugin.Instance.BuildManager.PlaceBuild(
                 buildName,
-                player.Player.transform.position,
-                player.Player.transform.rotation,
+                uPlayer.Player.transform.position,
+                uPlayer.Player.transform.rotation,
                 shift,
-                player.CSteamID.m_SteamID,
-                player.SteamGroupID.m_SteamID
+                playerId.m_SteamID,
+                groupId.m_SteamID
             );
 
             if (!buildFound)
-                ChatManager.serverSendMessage($"{buildName} does not exist", Color.yellow, toPlayer: player.SteamPlayer());
+            {
+                ChatManager.serverSendMessage($"{buildName} does not exist", Color.red, toPlayer: uPlayer.SteamPlayer());
+                return;
+            }
+        }
+
+        private CSteamID ParsePlayer(string name)
+        {
+            Player player = PlayerTool.getPlayer(name);
+
+            if (player == null)
+                return CSteamID.Nil;
+
+            return player.channel.owner.playerID.steamID;
+        }
+
+        private CSteamID ParseGroup(string name)
+        {
+            Player player = PlayerTool.getPlayer(name);
+
+            if (player == null)
+                return CSteamID.Nil;
+
+            return player.quests.groupID;
         }
     }
 }
